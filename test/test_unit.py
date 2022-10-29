fd15d403e1572bb358904fdf7b0a25bcbeb1820c
Nestor Flores <nestorchoele@gmail.com>
2022-10-29 13:42:18 -0300

    independent test

---
+++ test/test_unit.py
@@ -0,0 +1,40 @@
+# This sample code uses the Appium python client v2
+# pip install Appium-Python-Client
+# Then you can paste this into a file and simply run with Python
+
+from appium import webdriver
+from appium.webdriver.common.appiumby import AppiumBy
+
+# For W3C actions
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.actions import interaction
+from selenium.webdriver.common.actions.action_builder import ActionBuilder
+from selenium.webdriver.common.actions.pointer_input import PointerInput
+
+caps = {}
+caps["appium:deviceName"] = "Pixel 6 API 33"
+caps["appium:udid"] = "emulator-5554"
+caps["platformName"] = "Android"
+caps["appium:appPackage"] = "com.poincenot.bpn.mobile.qa"
+caps["appium:appActivity"] = "com.poincenot.doit.controller.activity.SplashActivity"
+caps["appium:autoGrantPermissions"] = "true"
+caps["appium:ensureWebviewsHavePages"] = True
+caps["appium:nativeWebScreenshot"] = True
+caps["appium:newCommandTimeout"] = 3600
+caps["appium:connectHardwareKeyboard"] = True
+
+driver = webdriver.Remote("http://localhost:4723/wd/hub", caps)
+
+driver.implicitly_wait(10)
+driver.update_settings({"waitForIdleTimeout": 100})
+
+el1 = driver.find_element(by=AppiumBy.XPATH, value="/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.EditText[1]")
+el1.send_keys("rioshugo")
+el2 = driver.find_element(by=AppiumBy.XPATH, value="/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.EditText[2]")
+el2.send_keys("QAbpn2022")
+el3 = driver.find_element(by=AppiumBy.XPATH, value="/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[6]/android.widget.TextView")
+el3.click()
+el4 = driver.find_element(by=AppiumBy.XPATH, value="/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup")
+el4.click()
+accept_button = driver.find_element(by=AppiumBy.XPATH, value="/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[4]")
+accept_button.click()
